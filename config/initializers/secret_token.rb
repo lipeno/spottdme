87d8a987c57496bdb99162b7bb2047d9b473b5a3
Viktor Fonic <viktor.fonic@gmail.com>
2013-03-27 14:05:04 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spottd::Application.config.secret_token = 'c76bb1b9050c3a0410a5e27cb9cea6b770e8012e0cfd4639d5f1648fd07f28f053c96b2fc52b2c9da1c41ec9f249b21c7c8ef27b04c5a0020e16ecd17b1482ba'
